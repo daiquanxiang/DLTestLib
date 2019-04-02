dd787c76ea779a780eb3de4b719ba5463df7cab5
David.Lik <919775874@qq.com>
2019-04-02 13:13:52 +0800

    edit

---
+++ upload_pod.sh
@@ -0,0 +1,27 @@
+
+#echo "${tagNum}"
+#chmod 777 upload_pod.sh
+
+
+#提交所有修改
+git add .
+
+git commit -m "edit"
+
+git push -u origin master
+
+#打tag
+git tag -a 0.1.5 -m "tag release 0.1.5"
+
+#上传
+git push --tags
+
+pod repo list
+
+pod repo remove DLTestLib
+
+pod repo add DLTestLib git@github.com:daiquanxiang/DLTestSpecs.git
+
+#上传文件ß
+#pod repo push WEHttpManager WEHttpManager.podspec --sources=git@120.26.233.28:WeSpec.git,http://git.wecut.com/WeSpec.git,https://github.com/CocoaPods/Specs.git --allow-warnings --verbose --use-libraries --skip-import-validation
+pod repo push DLTestLib DLTestLib.podspec  --allow-warnings --verbose --use-libraries --skip-import-validation
